aada87c040053f1a6938877a99266b6a0e0ab8a5
jembantonio <jembantonio@gmail.com>
2019-08-06 17:02:20 +0000

    fixed puppet styling

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Using Puppet, create a file in /tmp with requirements
 
 file { '/tmp/holberton':
-	ensure 	=> 'file',
-	path 	=> '/tmp/holberton',
-	mode 	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content	=> 'I love Puppet'
+  ensure  => 'file',
+  path 	  => '/tmp/holberton',
+  mode 	  => '0744',
+  owner	  => 'www-data',
+  group	  => 'www-data',
+  content => 'I love Puppet'
 }
