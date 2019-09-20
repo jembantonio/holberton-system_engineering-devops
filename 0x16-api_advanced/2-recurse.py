e2826c6ea7372948a3402cb04b0c72dad92175bf
jembantonio <jembantonio@gmail.com>
2019-09-20 06:07:07 +0000

    initial commit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit
+'''
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], name=None):
+    '''get request to find and sort hot posts recursively
+    '''
+    headers = {'User-agent': 'Mozilla/5.0'
+               'AppleWebKit/537.36'
+               'Chrome/76.0.3809.100 Safari/537.36'}
+
+    response = requests.get('https://www.reddit.com/r/{}/hot.json?after={}'
+                            .format(subreddit, name), headers=headers).json()
+    name = response.get('data', {}).get('after', None)
+    top = response.get('data', {}).get('children', [])
+
+    if not top:
+        return hot_list
+
+    for titles in top:
+        hot_list.append(titles.get('data').get('title'))
+
+    if not name:
+        return hot_list
+
+    return recurse(subreddit, hot_list, name)
