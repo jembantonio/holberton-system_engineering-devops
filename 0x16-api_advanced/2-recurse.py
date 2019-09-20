ba4c3a49f0c3e01ecf671d25d81d15909509ce80
jembantonio <jembantonio@gmail.com>
2019-09-20 06:11:41 +0000

    revert change

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -13,12 +13,6 @@ def recurse(subreddit, hot_list=[], name=None):
                'AppleWebKit/537.36'
                'Chrome/76.0.3809.100 Safari/537.36'}
 
-    checknopost = requests.get('https://reddit.com/api/search_reddit_names.json',
-                               headers=headers).json()
-
-    if 'error' in checknopost.keys()
-        return None    
-    
     response = requests.get('https://www.reddit.com/r/{}/hot.json?after={}'
                             .format(subreddit, name), headers=headers).json()
     name = response.get('data', {}).get('after', None)
