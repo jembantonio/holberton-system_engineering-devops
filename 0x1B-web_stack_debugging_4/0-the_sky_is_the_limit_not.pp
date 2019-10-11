1acc7ba520403bc927c5f28bd50a401ae65d966e
jembantonio <jembantonio@gmail.com>
2019-10-11 05:08:56 +0000

    initial commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,4 @@
+exec { '/etc/default/nginx':
+    command => 'sed -i "s/15/2000/" /etc/default/nginx && /etc/init.d/nginx restart',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
+}
